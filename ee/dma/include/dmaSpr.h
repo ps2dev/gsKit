e15686d2be14051fc621c9a644cc6c5cf86e3615
neovanglist <neovanglist@fd1d1198-7684-4768-b9be-5b3c44f97897>
2004-06-23 00:21:17 +0000

    Oops, forgot to add these.

---
+++ ee/dma/include/dmaSpr.h
@@ -0,0 +1,26 @@
+//  ____     ___ |    / _____ _____
+// |  __    |    |___/    |     |
+// |___| ___|    |    \ __|__   |     gsKit Open Source Project.
+// ----------------------------------------------------------------------
+// Copyright 2004 - Chris "Neovanglist" Gilbert <Neovanglist@LainOS.org>
+// Licenced under Academic Free License version 2.0
+// Review gsKit README & LICENSE files for further details.
+//
+// dmaSpr.h - Header for dmaSpr.c
+// Parts taken from emoon's BreakPoint Demo Library
+//
+
+#ifndef __DMASPR_H__
+#define __DMASPR_H__
+
+#include "dmaKit.h"
+
+#define SPR_START 0x70000000
+
+static void* DMA_SPR = (void*)SPR_START;
+
+void *dmaKit_spr_begin(void);
+int dmaKit_spr_end(void *data, void *StorePtr);
+void *dmaKit_spr_alloc(int size);
+
+#endif /* __DMASPR_H__ */
---
+++ ee/dma/src/dmaSpr.c
@@ -0,0 +1,42 @@
+//  ____     ___ |    / _____ _____
+// |  __    |    |___/    |     |
+// |___| ___|    |    \ __|__   |     gsKit Open Source Project.
+// ----------------------------------------------------------------------
+// Copyright 2004 - Chris "Neovanglist" Gilbert <Neovanglist@LainOS.org>
+// Licenced under Academic Free License version 2.0
+// Review gsKit README & LICENSE files for further details.
+//
+// dmaSpr.c - Interface to scratchpad.
+// Parts taken from emoon's BreakPoint Demo Library
+//
+
+#include "dmaKit.h"
+#include "kernel.h"
+
+void *dmaKit_spr_begin(void)
+{
+	printf("Begining Scratchpad Chain\n");
+	return (void *)dmaKit_spr_alloc(100 * 16);
+}
+
+int dmaKit_spr_end(void *data, void *StorePtr)
+{
+	printf("Ending Scratchpad Chain\n");
+	int q_size = (u32)data - (u32)StorePtr;
+	return q_size >> 4;
+}
+
+void *dmaKit_spr_alloc(int size)
+{
+	printf("Allocating Scratchpad Memory\n");
+	void *p_spr = DMA_SPR;
+	if( DMA_SPR + size >= ( ((void*)0x70000000)+16*1024 ) )
+		DMA_SPR = (void*)SPR_START;   
+
+	p_spr = DMA_SPR;
+	DMA_SPR += size;
+         
+	printf("Scratchpad Memory Allocated\n");
+	return p_spr;
+}
+
