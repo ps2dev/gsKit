0261c3888323c9588ffbb77c66e6088331ccf2ed
Rick Gaiser <rgaiser@gmail.com>
2016-11-10 21:23:47 +0100

    Add modetest example
    How to use:
    L1/R1:      Cycle through all video modes
    R3:         Change X/Y offset of screen
    X:          Switch between FIELD and FRAME mode
    LEFT/RIGHT: Change width in units of 64 pixels
    UP/DOWN:    Change height in units of 2 pixels
    
    Use ps2client to see the debug output, for example:
    Mode: 480i 576x452 GS_FIELD offset: -6, 0 memory: 576KiB

---
+++ examples/modetest/pad.c
@@ -0,0 +1,209 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2001-2004, ps2dev - http://www.ps2dev.org
+# Licenced under Academic Free License version 2.0
+# Review ps2sdk README & LICENSE files for further details.
+#
+# $Id$
+# Pad demo app
+# Quick and dirty, little or no error checks etc..
+# Distributed as is
+*/
+
+#include <tamtypes.h>
+#include <kernel.h>
+#include <sifrpc.h>
+#include <loadfile.h>
+#include <stdio.h>
+
+#include "libpad.h"
+
+/*
+ * Global var's
+ */
+// pad_dma_buf is provided by the user, one buf for each pad
+// contains the pad's current state
+static char padBuf[256] __attribute__((aligned(64)));
+
+static char actAlign[6];
+static int actuators;
+
+int port, slot;
+
+/*
+ * Local functions
+ */
+
+/*
+ * loadModules()
+ */
+static void
+loadModules(void)
+{
+    int ret;
+
+
+    ret = SifLoadModule("rom0:SIO2MAN", 0, NULL);
+    if (ret < 0) {
+        printf("sifLoadModule sio failed: %d\n", ret);
+        SleepThread();
+    }
+
+    ret = SifLoadModule("rom0:PADMAN", 0, NULL);
+    if (ret < 0) {
+        printf("sifLoadModule pad failed: %d\n", ret);
+        SleepThread();
+    }
+}
+
+/*
+ * waitPadReady()
+ */
+static int waitPadReady(int port, int slot)
+{
+    int state;
+    int lastState;
+    char stateString[16];
+
+    state = padGetState(port, slot);
+    lastState = -1;
+    while((state != PAD_STATE_STABLE) && (state != PAD_STATE_FINDCTP1)) {
+        if (state != lastState) {
+            padStateInt2String(state, stateString);
+            printf("Please wait, pad(%d,%d) is in state %s\n",
+                       port, slot, stateString);
+        }
+        lastState = state;
+        state=padGetState(port, slot);
+    }
+    // Were the pad ever 'out of sync'?
+    if (lastState != -1) {
+        printf("Pad OK!\n");
+    }
+    return 0;
+}
+
+
+/*
+ * initializePad()
+ */
+static int
+initializePad(int port, int slot)
+{
+
+    int ret;
+    int modes;
+    int i;
+
+    waitPadReady(port, slot);
+
+    // How many different modes can this device operate in?
+    // i.e. get # entrys in the modetable
+    modes = padInfoMode(port, slot, PAD_MODETABLE, -1);
+    printf("The device has %d modes\n", modes);
+
+    if (modes > 0) {
+        printf("( ");
+        for (i = 0; i < modes; i++) {
+            printf("%d ", padInfoMode(port, slot, PAD_MODETABLE, i));
+        }
+        printf(")");
+    }
+
+    printf("It is currently using mode %d\n",
+               padInfoMode(port, slot, PAD_MODECURID, 0));
+
+    // If modes == 0, this is not a Dual shock controller
+    // (it has no actuator engines)
+    if (modes == 0) {
+        printf("This is a digital controller?\n");
+        return 1;
+    }
+
+    // Verify that the controller has a DUAL SHOCK mode
+    i = 0;
+    do {
+        if (padInfoMode(port, slot, PAD_MODETABLE, i) == PAD_TYPE_DUALSHOCK)
+            break;
+        i++;
+    } while (i < modes);
+    if (i >= modes) {
+        printf("This is no Dual Shock controller\n");
+        return 1;
+    }
+
+    // If ExId != 0x0 => This controller has actuator engines
+    // This check should always pass if the Dual Shock test above passed
+    ret = padInfoMode(port, slot, PAD_MODECUREXID, 0);
+    if (ret == 0) {
+        printf("This is no Dual Shock controller??\n");
+        return 1;
+    }
+
+    printf("Enabling dual shock functions\n");
+
+    // When using MMODE_LOCK, user cant change mode with Select button
+    padSetMainMode(port, slot, PAD_MMODE_DUALSHOCK, PAD_MMODE_LOCK);
+
+    waitPadReady(port, slot);
+    printf("infoPressMode: %d\n", padInfoPressMode(port, slot));
+
+    waitPadReady(port, slot);
+    printf("enterPressMode: %d\n", padEnterPressMode(port, slot));
+
+    waitPadReady(port, slot);
+    actuators = padInfoAct(port, slot, -1, 0);
+    printf("# of actuators: %d\n",actuators);
+
+    if (actuators != 0) {
+        actAlign[0] = 0;   // Enable small engine
+        actAlign[1] = 1;   // Enable big engine
+        actAlign[2] = 0xff;
+        actAlign[3] = 0xff;
+        actAlign[4] = 0xff;
+        actAlign[5] = 0xff;
+
+        waitPadReady(port, slot);
+        printf("padSetActAlign: %d\n",
+                   padSetActAlign(port, slot, actAlign));
+    }
+    else {
+        printf("Did not find any actuators.\n");
+    }
+
+    waitPadReady(port, slot);
+
+    return 1;
+}
+
+void
+pad_init()
+{
+    int ret;
+
+    SifInitRpc(0);
+
+    loadModules();
+
+    padInit(0);
+
+    port = 0; // 0 -> Connector 1, 1 -> Connector 2
+    slot = 0; // Always zero if not using multitap
+
+    printf("PortMax: %d\n", padGetPortMax());
+    printf("SlotMax: %d\n", padGetSlotMax(port));
+
+
+    if((ret = padPortOpen(port, slot, padBuf)) == 0) {
+        printf("padOpenPort failed: %d\n", ret);
+        SleepThread();
+    }
+
+    if(!initializePad(port, slot)) {
+        printf("pad initalization failed!\n");
+        SleepThread();
+    }
+}
